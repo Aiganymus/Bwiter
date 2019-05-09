066d7652ae4c72c7d91a4ad13d489717a1d18696
Kuat Abylkassymov <kuatabylkasymov@gmail.com>
2019-05-09 22:00:35 +0600

    add registration

--- bwiter-app/src/app/app-routing.module.ts
+++ bwiter-app/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { BwitsTimelineComponent } from './bwits/bwits-timeline/bwits-timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'registration',
+  component: RegistrationComponent
+}, {
+  path: '',
+  component: BwitsTimelineComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bwiter-app/src/app/app.module.ts
+++ bwiter-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru-KZ';
@@ -8,21 +9,16 @@ import localeRu from '@angular/common/locales/ru-KZ';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BwitsModule } from './bwits/bwits.module';
-
+import { RegistrationComponent } from './registration/registration.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    BwitsModule
-  ],
+  declarations: [AppComponent, RegistrationComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BwitsModule, HttpClientModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
 
 registerLocaleData(localeRu);
