fc9364fb54ff5f57cda58088345d7d15c52eca1b
Kuat Abylkassymov <kuatabylkasymov@gmail.com>
2019-05-11 12:04:54 +0600

    add login and registration

--- bwiter-app/src/app/registration/registration.component.ts
+++ bwiter-app/src/app/registration/registration.component.ts
@@ -1,19 +1,16 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { FormBuilder,FormGroup , FormControl, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-
 interface Person {
-     joined_at: Date;
-     username: string;
-     nickname: string;
-     profile_pic: string;
-     status: string;
-     token:string;
-   }
-
-
+  joined_at: Date;
+  username: string;
+  nickname: string;
+  profile_pic: string;
+  status: string;
+  token: string;
+}
 
 @Component({
   selector: 'app-registration',
@@ -22,44 +19,38 @@ interface Person {
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
-
- @ViewChild('fileInput') fileInput: ElementRef;
-DJANGO_SERVER = 'http://127.0.0.1:8000'
+  @ViewChild('fileInput') fileInput: ElementRef;
+  DJANGO_SERVER = 'http://127.0.0.1:8000';
 
   form: FormGroup;
 
   loading = false;
 
-  constructor(private fb: FormBuilder, private http:HttpClient, private router:Router) {
-  }
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.createForm();
   }
 
   createForm() {
-      this.form = this.fb.group({
-        username: [""],
-        nickname: [""],
-        profile_pic: [""],
-        status: [""],
-        password: [""]
-      });
+    this.form = this.fb.group({
+      username: [''],
+      nickname: [''],
+      profile_pic: [''],
+      status: [''],
+      password: ['']
+    });
   }
 
   onFileChange(event) {
-    if(event.target.files.length > 0) {
+    if (event.target.files.length > 0) {
       let file = event.target.files[0];
       this.form.get('profile_pic').setValue(file);
     }
   }
 
-
   onSubmit() {
-
     let input = new FormData();
-
 
     console.log(this.form.get('username').value);
     console.log(this.form.get('nickname').value);
@@ -72,23 +63,23 @@ DJANGO_SERVER = 'http://127.0.0.1:8000'
     input.append('profile_pic', this.form.get('profile_pic').value);
     input.append('status', this.form.get('status').value);
     input.append('password', this.form.get('password').value);
-    this.form.get("username").setValue(null)
-    this.form.get("password").setValue(null)
-    this.form.get("nickname").setValue(null)
-    this.form.get("status").setValue(null)
-    this.form.get('profile_pic').setValue(null);
-    this.fileInput.nativeElement.value = '';
     this.loading = true;
     // In a real-world app you'd have a http request / service call here like
     // this.http.post('apiUrl', formModel)
 
     this.http.post<Person>('http://127.0.0.1:8000/api/users/', input).subscribe(
-      (res) => {
+      res => {
         console.log(res);
         this.loading = false;
-        this.router.navigateByUrl('login')
+        this.router.navigateByUrl('login');
+        this.form.get('username').setValue(null);
+        this.form.get('password').setValue(null);
+        this.form.get('nickname').setValue(null);
+        this.form.get('status').setValue(null);
+        this.form.get('profile_pic').setValue(null);
+        this.fileInput.nativeElement.value = '';
       },
-      (err) => {  
+      err => {
         console.log(err);
         this.loading = false;
       }
@@ -100,14 +91,5 @@ DJANGO_SERVER = 'http://127.0.0.1:8000'
     // }, 1000);
   }
 
-
-
-
-//   }
-
-
-
-
-
-
+  //   }
 }
