32206a7b1fe56321735140773588c81b40155209
darigas <daruga0608@gmail.com>
2019-05-13 16:00:45 +0600

    Conflicts figured out

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/darigummy/Documents/KBTU_WEB_2019/Bwiter/Bwiter/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
