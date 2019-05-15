713d40e24f4b0e2a3f4eea894c898e8f5d169e25
Kuat Abylkassymov <kuatabylkasymov@gmail.com>
2019-05-15 14:43:06 +0600

    add required=false, image

--- backend/bwits/serializers.py
+++ backend/bwits/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
-from accounts.serializers import UserSerializer,CustomUserSerializer
+from accounts.serializers import UserSerializer, CustomUserSerializer
 from .models import Bwit, LikeBwit
 from comments.serializers import CommentSerializer
 
 
 class BwitSerializerTwo(serializers.ModelSerializer):
     body = serializers.CharField(max_length=20)
-    picture = serializers.FileField()
+    picture = serializers.FileField(required=False)
     created_at = serializers.DateTimeField()
     author = UserSerializer(read_only=True)
+
     # bwit_likes = LikeBwitSerializer(read_only=True)
 
     class Meta:
@@ -25,6 +26,7 @@ class BwitSerializerTwo(serializers.ModelSerializer):
 class BwitSerializerTwo(serializers.ModelSerializer):
     body = serializers.CharField(max_length=20)
     picture = serializers.FileField()
+
     # created_at = serializers.DateTimeField()
     # author = UserSerializer(read_only=True)
 
@@ -37,6 +39,7 @@ class BwitSerializerTwo(serializers.ModelSerializer):
             # 'author',
         )
 
+
 # class LikeBwitSerializer(serializers.ModelSerializer):
 #     user = UserSerializer(read_only=True)
 #     bwit = BwitSerializerTwo(read_only=True)
@@ -45,7 +48,6 @@ class BwitSerializerTwo(serializers.ModelSerializer):
 #         model = LikeBwit
 #         # fields = ['user']
 #         fields = '__all__'
-
 
 
 class LikeBwitSerializer(serializers.Serializer):
@@ -79,5 +81,3 @@ class BwitSerializer(serializers.ModelSerializer):
             'bwit_likes',
             'bwit_comments',
         )
-
-
